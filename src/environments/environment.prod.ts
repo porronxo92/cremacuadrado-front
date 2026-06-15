dd0ce16a8cd8acaad449e600f836ba97008250a4
de la Cruz Diaz, Rubén <rcruzd@minsait.com>
2026-06-15 23:25:16 +0200

    publish Stripe y Google

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   mediaUrl: 'https://cremacuadrado-back.vercel.app',
   siteName: 'Cremacuadrado',
   siteDescription: 'Cremas de pistacho artesanales de La Mancha',
-  stripePublishableKey: '',  // Fill with your pk_live_... from Stripe Dashboard
-  googleClientId: '',  // Fill with your Google OAuth 2.0 Client ID for production domain
+  stripePublishableKey: 'pk_test_51ThaYH73CZ1Cgk4GE8HA4vpmQpDIpzlntQ501WxPzhDYd6Y4ZdBTxYEtjO1xVQqrE6sBoPdBZvQKRMh7peciV04Q00VOAzEYbF',  // Fill with your pk_live_... from Stripe Dashboard
+  googleClientId: '170670252595-4ihcb1du6rbq548vdiu6ll6m602cr7s9.apps.googleusercontent.com',  // Fill with your Google OAuth 2.0 Client ID for production domain
 };
